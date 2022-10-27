c12ff599cfeaa991fdb52b350b9ba0c6cd0f3fab
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-27 15:03:00 -0300

    Update Similar.py

--- Similar.py
+++ Similar.py
@@ -245,7 +245,7 @@ st.write(df_jogs)
 df_stats = base_comp[base_comp.columns.tolist()[8:]].copy().subtract(lista_valores)
 v = 0
 for coluna in categorias:
-  df_stats[coluna] = df_stats[coluna]/lista_valores[v]
+  df_stats[coluna] = abs(df_stats[coluna]/lista_valores[v])
   v += 1
 
 df_stats['Jogador'] = base_comp['Jogador']
