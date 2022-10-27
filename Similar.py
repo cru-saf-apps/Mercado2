2f5dd2eb9110f712da4a00fe50e2325432cb21fd
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-27 16:32:50 -0300

    Update Similar.py

--- Similar.py
+++ Similar.py
@@ -265,7 +265,7 @@ while t < len(df_jogs):
   aux_df = base_comp[(base_comp.Jogador == df_jogs.Jogador[t])&(base_comp['Equipe atual']==df_jogs['Equipe atual'][t])]
   lista_difers = []
   v=0
-  for coluna in aux_df.columns.tolist()[9:]:
+  for coluna in aux_df.columns.tolist()[-len(lista_valores):]:
     if coluna in vars_abs:
       soma = np.nansum(aux_df[coluna])
     else:
