359fc0e9f4735bd61a58ac8f3d87a43c28cdee06
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-27 21:02:57 -0300

    Update Similar.py

--- Similar.py
+++ Similar.py
@@ -269,7 +269,7 @@ while t < len(df_jogs):
       soma = np.nansum(aux_df[coluna])
     else:
       soma = np.nanmean(aux_df[coluna])
-    df_jogs[coluna][t] = abs((soma-lista_valores[v])/(np.nanmax(df_comp[coluna]) - np.nanmin(df_comp[coluna]))
+    df_jogs[coluna][t] = abs((soma-lista_valores[v])/(np.nanmax(df_comp[coluna]) - np.nanmin(df_comp[coluna])))
       
   t += 1
 
